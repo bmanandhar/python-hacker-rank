7d86c8c1f183c50a3cee84a549742123ddfdc979
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-09-21 08:04:04 -0700

    adds HackerRank

--- left_rotation.py
+++ left_rotation.py
@@ -6,19 +6,48 @@ Created on Fri Sep 20 21:18:52 2019
 @author: bijayamanandhar
 """
 
+"""
+A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become .
 
+Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+
+Function Description
+
+Complete the function rotLeft in the editor below. It should return the resulting array of integers.
+
+rotLeft has the following parameter(s):
+
+An array of integers .
+An integer , the number of rotations.
+Input Format
+
+The first line contains two space-separated integers  and , the size of  and the number of left rotations you must perform.
+The second line contains  space-separated integers .
+
+Constraints
+
+Output Format
+
+Print a single line of  space-separated integers denoting the final state of the array after performing  left rotations.
+
+Sample Input
+
+5 4
+1 2 3 4 5
+Sample Output
+
+5 1 2 3 4
+Explanation
+
+When we perform  left rotations, the array undergoes the following sequence of changes:
+
+
+"""
 def rotLeft(a, d):
 
-    #Creates a variable for the result
-    result = []
-
-    #Moving the elements to its left in a new array     
-    for i in range(0, d):
-        
-        result.append(a[i])
-        
-    return a[(d):len(a)] + result
-    
+    #Moving the elements to its left
+       
+    return a[d:len(a)] + a[:d]
     
     
 print(rotLeft([1,2,3,4,5], 4) == [5, 1, 2, 3, 4]) #Should print 'True'
