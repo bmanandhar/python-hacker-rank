4c5efa11820fe00aa72644bda6bfe84fcc8201c7
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-10-11 16:51:59 -0700

    picking numbers

--- page_count.py
+++ page_count.py
@@ -6,24 +6,73 @@ Created on Sat Sep 21 18:18:32 2019
 @author: bijayamanandhar
 """
 
+"""
+Brie’s Drawing teacher asks her class to open their books to a page number. Brie can either start turning pages from the front of the book or from the back of the book. She always turns pages one at a time. When she opens the book, page  is always on the right side:
+
+image
+
+When she flips page , she sees pages  and . Each page except the last page will always be printed on both sides. The last page may only be printed on the front, given the length of the book. If the book is  pages long, and she wants to turn to page , what is the minimum number of pages she will turn? She can start at the beginning or the end of the book.
+
+Given  and , find and print the minimum number of pages Brie must turn in order to arrive at page .
+
+Function Description
+
+Complete the pageCount function in the editor below. It should return the minimum number of pages Brie must turn.
+
+pageCount has the following parameter(s):
+
+n: the number of pages in the book
+p: the page number to turn to
+Input Format
+
+The first line contains an integer , the number of pages in the book.
+The second line contains an integer, , the page that Brie's teacher wants her to turn to.
+
+Constraints
+
+Output Format
+
+Print an integer denoting the minimum number of pages Brie must turn to get to page .
+
+Sample Input 0
+
+6
+2
+Sample Output 0
+
+1
+Explanation 0
+
+If Brie starts turning from page , she only needs to turn  page:
+"""
+
+
 def pageCount(n, p):
     #
     # Write your code here.
     #
     
-    #If number of pages is even, add 1 to it to make it 
+    #If number of pages is even, add 1
     if n % 2 == 0:
         n += 1
     
-    
     front, back = None, None
+    
+    
+    #Number of pages to turn from front
+    #Turning pages from front has nothing to do with total pages
+    #Because it starts from page-1
     front = p//2
+
+    #Number of pages to turn from back    
     back = (n-p)//2
 
+    #Check which is smaller
     if front < back or front == back:
         return front
+    
     else:
         return back
     
-print(pageCount(6, 6))
+print(pageCount(6, 5) == 1)#Prints 'True'
 
---
+++ picking_numbers.py
@@ -0,0 +1,65 @@
+'''
+Given an array of integers, find and print the maximum number of integers you can select from the array such that the absolute difference between any two of the chosen integers is less than or equal to . For example, if your array is , you can create two subarrays meeting the criterion:  and . The maximum length subarray has  elements.
+
+Function Description
+
+Complete the pickingNumbers function in the editor below. It should return an integer that represents the length of the longest array that can be created.
+
+pickingNumbers has the following parameter(s):
+
+a: an array of integers
+Input Format
+
+The first line contains a single integer , the size of the array .
+The second line contains  space-separated integers .
+
+Constraints
+
+The answer will be .
+Output Format
+
+A single integer denoting the maximum number of integers you can choose from the array such that the absolute difference between any two of the chosen integers is .
+
+Sample Input 0
+
+6
+4 6 5 3 3 1
+Sample Output 0
+
+3
+Explanation 0
+
+We choose the following multiset of integers from the array: . Each pair in the multiset has an absolute difference  (i.e.,  and ), so we print the number of chosen integers, , as our answer.
+
+Sample Input 1
+
+6
+1 2 2 3 1 2
+Sample Output 1
+
+5
+Explanation 1
+
+We choose the following multiset of integers from the array: . Each pair in the multiset has an absolute difference  (i.e., , , and ), so we print the number of chosen integers, , as our answer.
+
+'''
+
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    # Initialization of values
+    n, n_i, n_j = 0, 0, 0  
+    set_a = set(a)
+    
+    for i in set_a:
+        for j in range(len(a)):
+            if abs(i-a[j]) == 1:
+                n_i, n_j = a.count(i), a.count(a[j])
+                if n_i + n_j > n:
+                    n = n_i + n_j
+                    n_i, n_j = 0, 0
+    return n 
